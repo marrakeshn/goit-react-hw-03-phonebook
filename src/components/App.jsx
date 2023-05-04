484adfff4ca4bc95ae8d2e90f8319f84db324ee6
marrakeshn <nesveteymark@gmail.com>
2023-05-04 14:02:43 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,12 +8,7 @@ import Notification from './Notification';
 
 class App extends Component {
   state = {
-    contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -23,8 +18,8 @@ class App extends Component {
       name,
       number,
     };
-    this.state.contacts.some(el => el.name === contact.name)
-      ? alert(`${name} is already in contacts`)
+    this.state.contacts.some((el => el.name.toLowerCase() === contact.name.toLowerCase()) && (el => el.number.toLowerCase() === contact.number.toLowerCase()))
+      ? alert((`${name} is already in contacts`) && (`${number} is already in contacts`))
       : this.setState(({ contacts }) => ({
           contacts: [contact, ...contacts],
         }));
